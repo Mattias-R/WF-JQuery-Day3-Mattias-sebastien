a40902f365eab4579ce547b2d3931e6ae7e86f58
Sébastien Pflanzl <sv.pflanzl@gmail.com>
2019-10-23 10:48:32 +0200

    interm

---
+++ iex/script.js
@@ -0,0 +1,3 @@
+$(".product-image[data-product-type=phone]").css("background", "lightgreen")
+$(".product-image[data-product-type=tablet]").css("background", "lightblue")
+$(".product-image[data-product-type=notebook]").css("background", "lightpink")
